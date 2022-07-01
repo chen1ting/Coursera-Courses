dbf9c565f5760396cd14ea60eccff3847981a277
Chen Yiting <80753389+chen1ting@users.noreply.github.com>
2022-07-01 12:18:44 +0800

    ex6-octave updated and finished first half

---
+++ Stanford-CS299-MachineLearning/ex1-ex8-octave/ex6-octave/dataset3Params.m
@@ -0,0 +1,48 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 0.01;
+sigma = 0.01;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example,
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using
+%        mean(double(predictions ~= yval))
+%
+
+% use the parameter suggested in the pdf
+C_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_list = [0.01 0.03 0.1 0.3 1 3 10 30];
+min_error = Inf
+for i=1:length(C_list) % try each combination from two lists
+  for j=1:length(sigma_list)
+    C_test = C_list(i);
+    sigma_test = sigma_list(i);
+    model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    if prediction_error < min_error
+      C = C_test;
+      sigma = sigma_test;
+      min_error = prediction_error;
+    endif
+  endfor
+endfor
+
+
+% =========================================================================
+
+end
