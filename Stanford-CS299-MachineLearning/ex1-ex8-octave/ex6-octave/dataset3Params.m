5773844376fdc3583b7f5125906b311e0f56c872
Chen Yiting <80753389+chen1ting@users.noreply.github.com>
2022-07-01 14:06:43 +0800

    Finished Wk7 Homework

--- Stanford-CS299-MachineLearning/ex1-ex8-octave/ex6-octave/dataset3Params.m
+++ Stanford-CS299-MachineLearning/ex1-ex8-octave/ex6-octave/dataset3Params.m
@@ -24,25 +24,33 @@ sigma = 0.01;
 %
 
 % use the parameter suggested in the pdf
-C_list = [0.01 0.03 0.1 0.3 1 3 10 30];
-sigma_list = [0.01 0.03 0.1 0.3 1 3 10 30];
-min_error = Inf
-for i=1:length(C_list) % try each combination from two lists
-  for j=1:length(sigma_list)
-    C_test = C_list(i);
-    sigma_test = sigma_list(i);
-    model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
-    predictions = svmPredict(model, Xval);
-    prediction_error = mean(double(predictions ~= yval));
-    if prediction_error < min_error
-      C = C_test;
-      sigma = sigma_test;
-      min_error = prediction_error;
-    endif
-  endfor
-endfor
+C_list     = [0.01 0.03 0.1 0.3 1 3 10 30]';
+sigma_list = [0.01 0.03 0.1 0.3 1 3 10 30]';
 
+prediction_error = zeros(length(C_list), length(sigma_list));
+result = zeros(length(C_list)+length(sigma_list),3);
+row = 1;
 
+for i = 1:length(C_list)
+    for j = 1: length(sigma_list)
+        C_test = C_list(i);
+        sigma_test = sigma_list(j);
+
+        model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
+        predictions = svmPredict(model, Xval);
+        prediction_error(i,j) = mean(double(predictions ~= yval));
+
+        result(row,:) = [prediction_error(i,j), C_test, sigma_test];
+        row = row + 1;
+    end
+end
+
+% Sorting prediction_error in ascending order
+sorted_result = sortrows(result, 1);
+
+% C and sigma corresponding to min(prediction_error)
+C = sorted_result(1,2);
+sigma = sorted_result(1,3);
 % =========================================================================
 
 end
